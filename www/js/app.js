10b72bea1d3936d5e70806d4aed32fc4cb97e0bf
Chris <kirisu@gmail.com>
2015-05-16 04:20:04 -0700

    adding all libs and minor attempts at speeding up the ui

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('ceaseless', ['ionic', 'ceaseless.controllers', 'ceaseless.services', 'ios7login'])
+angular.module('ceaseless', ['ionic', 'ceaseless.controllers', 'ceaseless.services', 'ios7login', 'QuickList'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
