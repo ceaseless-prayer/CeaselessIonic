fd809ed27cb6286a422bc7eb61b9ed1c990ce284
Chris <kirisu@gmail.com>
2015-07-29 18:03:37 -0700

    intermediate step to getting functioning people view

--- www/js/controllers/PeopleController.js
+++ www/js/controllers/PeopleController.js
@@ -1,23 +1,27 @@
 angular.module('ceaseless.controllers')
 
-  .controller('PeopleCtrl', function($scope, background, $ionicPlatform, $cordovaContacts, PeopleService) {
+  .controller('PeopleCtrl', function($scope, background, $stateParams, $ionicPlatform, $cordovaContacts, PeopleService) {
     $scope.userMsg = 'waiting for platform readiness';
     $scope.contacts = [];
     $scope.backgroundStyles = background.styles;
+    $scope.active = true;
+    $scope.shouldShowDelete = false;
+    $scope.listCanSwipe = true;
 
     $ionicPlatform.ready(function() {
       $scope.userMsg = 'platform ready';
       if (!navigator.contacts) {
         console.log('Cordova contacts not available.')
       } else {
+
         $scope.getContactList = function() {
           $scope.userMsg = 'fetching';
           var fields = ['displayName', 'name', 'photos'];
           $cordovaContacts.find({filter: '', fields: fields}).then(function(result) {
             $scope.userMsg = 'in process';
-            var contacts = _.filter(result, function (c) {
-              return c.name.formatted !== '';
-            });
+            var contacts = _.sortBy(_.filter(result, function (c) {
+              return c.name.formatted !== '' && !_.startsWith(c.name.formatted, '#');
+            }), 'displayName');
 
             for(var i in contacts) {
               var c = contacts[i];
@@ -35,6 +39,14 @@ angular.module('ceaseless.controllers')
             $scope.userMsg = error;
           });
         };
+        $scope.initContacts = function () {
+          PeopleService.init();
+        };
+        $scope.getContactsFromDB = function () {
+          PeopleService.list().then(function (contacts) {
+            $scope.contacts = contacts;
+          });
+        }
       }
     });
 
--- www/js/services/PeopleService.js
+++ www/js/services/PeopleService.js
@@ -1,11 +1,17 @@
 angular.module('ceaseless.services')
-  .factory('PeopleService', function ($cordovaSQLite, AppConstants, ceaselessServiceUrls) {
-//    var db = $cordovaSQLite.openDB(AppConstants.databaseName);
-//    db.transaction(function (tx) {
-//      tx.executeSql('CREATE TABLE IF NOT EXISTS PersonIdentifier (ceaselessId text primary key, favoritedDate text, lastInvitedDate text, removedDate text, systemRemovedDate text)');
-//      tx.executeSql('CREATE TABLE IF NOT EXISTS PersonInfo (id integer primary key, text text, createDate text, lastUpdatedDate text)');
-//      tx.executeSql('CREATE TABLE IF NOT EXISTS PrayerRecord (id integer primary key, createDate text, type text, person text)');
-//    });
+  .factory('PeopleService', function ($ionicPlatform, $cordovaSQLite, AppConstants, ceaselessServiceUrls, $cordovaContacts) {
+    var db = $cordovaSQLite.openDB(AppConstants.databaseName);
+
+    function setupPeopleEntity (db) {
+      db.transaction(function (tx) {
+        tx.executeSql('DROP TABLE IF EXISTS testPersonTable');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS testPersonTable (ceaselessId text primary key, displayName text, profilePicture text, initials varchar(4))');
+//        tx.executeSql('CREATE TABLE IF NOT EXISTS PersonIdentifier (ceaselessId text primary key, favoritedDate text, lastInvitedDate text, removedDate text, systemRemovedDate text)');
+//        tx.executeSql('CREATE TABLE IF NOT EXISTS PersonInfo (id integer primary key, text text, createDate text, lastUpdatedDate text)');
+//        tx.executeSql('CREATE TABLE IF NOT EXISTS PrayerRecord (id integer primary key, createDate text, type text, person text)');
+      });
+    }
+
 
     function objectFromRow (row) {
       var personObj = {};
@@ -26,20 +32,86 @@ angular.module('ceaseless.services')
       return personObj;
     }
 
+    function syncWithContacts() {
+      console.log('called sync');
+      $ionicPlatform.ready(function() {
+        if (!navigator.contacts) {
+          console.log('Cordova contacts not available.')
+        } else {
+          var syncContactList = function() {
+            console.log('syncing contacts');
+
+            var fields = ['displayName', 'name', 'photos'];
+            $cordovaContacts.find({filter: '', fields: fields}).then(function(result) {
+              var contacts = _.sortBy(_.filter(result, function (c) {
+                return c.name.formatted !== ''
+                  && !_.startsWith(c.name.formatted, '#')
+                  && !isEmail(c.name.formatted);
+              }), 'displayName');
+
+              console.log('creating contacts');
+              for(var i in contacts) {
+                var c = contacts[i];
+                if(c.photos) {
+                  c.profilePicture = c.photos[0].value;
+                }
+                c.initials = getInitialsForContact(c);
+                saveContact(c);
+              }
+            }, function(error) {
+              console.log('ERROR', error);
+            });
+            console.log('done syncing');
+          };
+          syncContactList();
+        }
+      });
+    }
+
+    function contactFromRow (row) {
+      var contactObj = {};
+      contactObj.ceaselessId = row.ceaselessId;
+      contactObj.displayName = row.displayName;
+      contactObj.profilePicture = row.profilePicture;
+      contactObj.initials = row.initials;
+      return contactObj;
+    }
+
     function listPeople() {
-      var query = 'SELECT * FROM PersonIdentifier';
+      // var query = 'SELECT * FROM PersonIdentifier';
+      var query = 'SELECT * FROM testPersonTable';
+      return $cordovaSQLite.execute(db, query).then(function (res) {
+        var len = res.rows.length;
+        var contactObjects = [];
+        if (len > 0) {
+          for(var i = 0; i < len; i++) {
+            contactObjects.push(contactFromRow(res.rows.item(i)));
+          }
+        }
+        return contactObjects;
+      });
     }
 
     function getContact(id) {
       var query = 'SELECT * FROM PersonIdentifier WHERE id = ?';
+
     }
 
     function saveContact(contact) {
-      var query = '';
+      if(contact.ceaselessId) {
+        var query = 'UPDATE testPersonTable SET displayName = ?, profilePicture = ?, initials = ? WHERE ceaselessId = ?';
+        return $cordovaSQLite.execute(db, query, [contact.displayName, contact.profilePicture, contact.initials, contact.ceaselessId]);
+      } else {
+        console.log('generating new uuid for contact');
+        var id = uuid.v4();
+        var query = 'INSERT INTO testPersonTable (ceaselessId, displayName, profilePicture, initials) VALUES (?,?,?,?)';
+        return $cordovaSQLite.execute(db, query, [id, contact.displayName, contact.profilePicture, contact.initials]);
+      }
     }
 
     function deleteContact(contact) {
       var query = '';
+
     }
 
     function getInitialsForContact(contact) {
@@ -48,7 +120,17 @@ angular.module('ceaseless.services')
       return givenName.substring(0,1) + familyName.substring(0,1);
     }
 
+    function isEmail(input) {
+      var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+      return emailRegex.test(input.trim());
+    }
+
     return {
-      getInitialsForContact: getInitialsForContact
+      getInitialsForContact: getInitialsForContact,
+      init: function () {
+        setupPeopleEntity(db);
+        syncWithContacts();
+      },
+      list: listPeople
     };
   });
